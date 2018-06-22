49571ae89a2f82fc116b96993abd5bf34714b2c6
ebosi38051 <ebosi38051@gmail.com>
2018-06-22 22:14:53 +0900

    Implement Input text

--- Main.elm
+++ Main.elm
@@ -1,30 +1,43 @@
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 
 main =
   Html.beginnerProgram { model = model, view = view, update = update}
 
-type alias Model = Int
+type alias Model =
+  { counter: Int
+  , content: String
+  }
 
 model : Model
 model =
-  0
+  { counter = 0
+  , content = ""
+  }
 
-type Msg = Increment | Decrement
+type Msg =
+  Increment
+  | Decrement
+  | Change String
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
+    Change str ->
+      { model | content = str }
     Increment ->
-      model + 1
+      { model | counter = model.counter + 1 }
     Decrement ->
-      model - 1
+      { model | counter = model.counter - 1 }
 
 view : Model -> Html Msg
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Decrement ] [ text "+" ]
+    , div [] [ text (toString model.counter) ]
+    , button [ onClick Increment ] [ text "+" ]
+    , input [ placeholder "Text to reverse", onInput Change] []
+    , div [] [ text (toString model.content) ]
     ]
 
